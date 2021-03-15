5f07a161d569a668d15ddb11bb04bf263a1558c7
sasaafanasienka <mark0182@tut.by>
2021-03-15 19:14:56 +0300

    state removed from app component

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import './App.css'
 import '../styles/anyClassName.css'
 import Carousel from "./Carousel/Carousel"
@@ -19,25 +19,25 @@ function App() {
         return Math.floor(min + Math.random() * Math.floor(max + 1 - min));
     }
 
-    const[state, setState] = useState({
+    const config = {
         content: getRandom(3,10),
         gap: getRandom(0,4) * 5,
         itemsPerView: getRandom(3,5),
         loop: getRandom(0,1) === 0 ? true : false
-    })
+    }
 
     return (
         <div className='App'>
-            <h1 className='text'>Number of items: {state.content}</h1>
-            <h1 className='text'>Gap: {state.gap}px</h1>
-            <h1 className='text'>Items per view: {state.itemsPerView}</h1>
-            <h1 className='text'>Loop: {state.loop.toString()}</h1>
+            <h1 className='text'>Number of items: {config.content}</h1>
+            <h1 className='text'>Gap: {config.gap}px</h1>
+            <h1 className='text'>Items per view: {config.itemsPerView}</h1>
+            <h1 className='text'>Loop: {config.loop.toString()}</h1>
             <h1 className='text'>Current settings was generated randomly. If you need to check how it works with other settings just refresh the page</h1>
             <Carousel
-                content={makeContentArr(state.content)}
-                gap={state.gap}
-                itemsPerView={state.itemsPerView}
-                loop={state.loop}
+                content={makeContentArr(config.content)}
+                gap={config.gap}
+                itemsPerView={config.itemsPerView}
+                loop={config.loop}
             />
         </div>
     );
